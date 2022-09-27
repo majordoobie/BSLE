e14444559e8c9c947c3fe30930619356e25ecbb4
doobie <sgtmajordoobie@live.com>
2022-09-27 17:53:36 -0400

    [Modified gtest_server_file_api.cpp] Added some atomic variagbles to help synchronize the threads from ctest -j $(nproc)

--- test/server_tests/gtest_server_file_api.cpp
+++ test/server_tests/gtest_server_file_api.cpp
@@ -1,7 +1,10 @@
 #include <gtest/gtest.h>
 #include <server_file_api.h>
-#include <libgen.h>
 #include <stdio.h>
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+#include <atomic>
 
 extern "C"
 {
@@ -12,47 +15,34 @@ extern "C"
     };
 }
 
-static bool run_tests = true;
+// Variables are used to synchronize the threads in ctest -j $(nproc)
+static volatile std::atomic_uint clear = 0;
+static unsigned int tests = 2;
 
-TEST(TestForTestFiles, Test)
+/*!
+ * Create the test files if they do not exist
+ */
+void init_test_dir(void)
 {
-    // Creates the test files for this unit test
-    //mkdir -p /tmp/dir/another_dir; touch /tmp/dir/somefile.txt; touch /tmp/otherfile.txt";
-    if (-1 == mkdir("/tmp/dir", 0777))
-    {
-        if (EEXIST != errno)
-        {
-            run_tests = false;
-            ASSERT_TRUE(run_tests) << "Could not create test files\n";
-        }
-    }
-    if (-1 == mkdir("/tmp/dir/another_dir", 0777))
-    {
-        if (EEXIST != errno)
-        {
-            perror("mkdir");
-            run_tests = false;
-            ASSERT_TRUE(run_tests) << "Could not create test files\n";
-        }
-    }
-    FILE * f = fopen("/tmp/dir/somefile.txt", "w");
-    if (NULL == f)
-    {
-        perror("open");
-        run_tests = false;
-        ASSERT_TRUE(run_tests) << "Could not create test files\n";
-    }
-    fclose(f);
-    f = fopen("/tmp/dir/another_dir/another_file.txt", "w");
-    if (NULL == f)
-    {
-        perror("open");
-        run_tests = false;
-        ASSERT_TRUE(run_tests) << "Could not create test files\n";
-    }
-    fclose(f);
-}
+    const std::filesystem::path test_dir{"/tmp/dir"};
 
+    if (!std::filesystem::exists(test_dir))
+    {
+        std::filesystem::create_directory(test_dir);
+    }
+    if (!std::filesystem::exists(test_dir/"another_dir"))
+    {
+        std::filesystem::create_directory(test_dir/"another_dir");
+    }
+    if (!std::filesystem::exists(test_dir/"somefile.txt"))
+    {
+        std::ofstream {test_dir/"somefile.txt"};
+    }
+    if (!std::filesystem::exists(test_dir/"another_dir/another_file.txt"))
+    {
+        std::ofstream{test_dir/"another_dir/another_file.txt"};
+    }
+}
 class ServerFileApiTest : public ::testing::TestWithParam<std::tuple<std::string, std::string, std::string, bool, bool, bool>>{};
 
 /*
@@ -64,68 +54,83 @@ class ServerFileApiTest : public ::testing::TestWithParam<std::tuple<std::string
  */
 TEST_P(ServerFileApiTest, TestFileJoining)
 {
-    if (!run_tests)
-    {
-        GTEST_SKIP_("Did not find test files\n");
-    }
+    init_test_dir();
+
     auto [parent, child, expected_str, b_expect_resolve, expect_find, expect_may_exist] = GetParam();
-    verified_path_t * p_path = f_path_resolve(parent.c_str(), child.c_str());
 
-    if (b_expect_resolve)
+    // Cleaup if the tests are over
+    if (parent == "NOT_TEST")
     {
-        ASSERT_NE(nullptr, p_path) << "\n[!!] Create test files; mkdir -p /tmp/dir/another_dir; touch /tmp/dir/somefile.txt; touch /tmp/otherfile.txt";
-        EXPECT_EQ(0, strcmp(expected_str.c_str(), p_path->p_path));
-
-
-        FILE * h_file = f_open_file(p_path, "r");
-
-        if (expect_find)
+        long long res = clear.fetch_add(1);
+        if (res >= tests)
         {
-            EXPECT_NE(nullptr, h_file);
+            std::filesystem::remove_all("/tmp/dir");
         }
-        else
-        {
-            EXPECT_EQ(nullptr, h_file);
-        }
-        if (NULL != h_file)
-        {
-            fclose(h_file);
-        }
-        f_destroy_path(&p_path);
     }
     else
     {
-        EXPECT_EQ(nullptr, p_path);
+        verified_path_t * p_path = f_path_resolve(parent.c_str(), child.c_str());
+
+        if (b_expect_resolve)
+        {
+            ASSERT_NE(nullptr, p_path) << "\n[!!] Create test files; mkdir -p /tmp/dir/another_dir; touch /tmp/dir/somefile.txt; touch /tmp/otherfile.txt";
+            EXPECT_EQ(0, strcmp(expected_str.c_str(), p_path->p_path));
+
+
+            FILE * h_file = f_open_file(p_path, "r");
+
+            if (expect_find)
+            {
+                EXPECT_NE(nullptr, h_file);
+            }
+            else
+            {
+                EXPECT_EQ(nullptr, h_file);
+            }
+            if (NULL != h_file)
+            {
+                fclose(h_file);
+            }
+            f_destroy_path(&p_path);
+        }
+        else
+        {
+            EXPECT_EQ(nullptr, p_path);
+        }
+        free(p_path);
     }
-
-
-    free(p_path);
 }
 
 TEST_P(ServerFileApiTest, TestFileMayExist)
 {
-    if (!run_tests)
-    {
-        GTEST_SKIP_("Did not find test files\n");
-    }
+    init_test_dir();
 
     auto [parent, child, expected_str, b_expect_resolve, expect_find, expect_may_exist] = GetParam();
 
-    static int val = 0;
-    val++;
-
-    verified_path_t * p_path =
-        f_valid_resolve((char *)parent.c_str(), (char *)child.c_str());
-    if (expect_may_exist)
+    // Cleaup if the tests are over
+    if (parent == "NOT_TEST")
     {
-        EXPECT_NE(nullptr, p_path);
-        f_destroy_path(&p_path);
+        long long res = clear.fetch_add(1);
+        if (res >= tests)
+        {
+            std::filesystem::remove_all("/tmp/dir");
+        }
     }
+
     else
     {
-        EXPECT_EQ(nullptr, p_path);
+        verified_path_t * p_path =
+            f_valid_resolve((char *)parent.c_str(), (char *)child.c_str());
+        if (expect_may_exist)
+        {
+            EXPECT_NE(nullptr, p_path);
+            f_destroy_path(&p_path);
+        }
+        else
+        {
+            EXPECT_EQ(nullptr, p_path);
+        }
     }
-
 }
 
 INSTANTIATE_TEST_SUITE_P(
@@ -143,5 +148,6 @@ INSTANTIATE_TEST_SUITE_P(
         std::make_tuple("/tmp/dir/", "test file", "", false, false, true),
         std::make_tuple("/tmp/dir/", "no_exist.txt", "/tmp/dir/no_exists.txt", false, false, true),
         std::make_tuple("/tmp/dir/", "/another_dir/no_exist.txt", "/tmp/dir/no_exists.txt", false, false, true),
-        std::make_tuple("/tmp/dir", "../dir/somefile.txt", "/tmp/dir/somefile.txt", true, true, true)
+        std::make_tuple("/tmp/dir", "../dir/somefile.txt", "/tmp/dir/somefile.txt", true, true, true),
+        std::make_tuple("NOT_TEST", "NOT_TEST", "NOT_TEST", true, true, true)
     ));
